06e79c9328441fea97eded5bc6d94964d318a7ff
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-08 12:24:20 +0530

    gql

--- index.js
+++ index.js
@@ -2,26 +2,53 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+
+// dotenv
+
+require('dotenv').config()
+const Post = require('./Model/Post')
+
+
 const typeDefs = gql`
+    type Post{
+        id : ID!
+        body : String!
+        createdAt : String!
+        username : String!
+    }
     type Query{
-        sayHi : String!
+        getPosts : [Post]
     }
 `
 
-
 const resolvers = {
-    Query : {
-        sayHi : () => 'Hello World!'
+    Query: {
+       async getPosts(){
+            try {
+               const posts = await Post.find()
+               return posts 
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
     }
 }
+
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database conected'))
+    .catch(e => console.log(e))
 
-server.listen({ port : 5000})
-.then(res => {
-    console.log(`server runing at ${res.url}`)
-})
+
+
+const port = process.env.PORT || 5000
+
+server.listen({ port })
+    .then(res => {
+        console.log(`server runing at ${res.url}`)
+    })
