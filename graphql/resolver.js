537e297c56a71290c23335fc21580b4c51aa1d6e
Arun Singh <arunsingh2aug.as@gmail.com>
2021-05-30 00:07:54 +0530

    creating Event

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,5 +1,6 @@
-
+const bcrypt = require('bcryptjs')
 const _event = require('../model/event')
+const _user = require('../model/user')
 
 module.exports = {
     events: async () => {
@@ -15,13 +16,37 @@ module.exports = {
             title: arg.eventInput.title,
             description: arg.eventInput.description,
             price: +arg.eventInput.price,
-            date: new Date(arg.eventInput.date)
+            date: new Date(arg.eventInput.date),
+            creator: 'asdfa13adf21212sss1'
         })
         return event.save()
             .then(result => {
-                console.log(result)
+                
                 return { ...result._doc }
+            })
+            .catch(err => console.log(err))
+    },
+    createUser: (arg) => {
+        return _user.findOne({ email: arg.userInput.email })
+            .then(user => {
+                if (user) {
+                    throw new Error("user already exists")
+                }
+                return bcrypt.hash(arg.userInput.password, 10)
+            })
+            .then(hashedPassword => {
+                const user = new _user({
+                    email: arg.userInput.email,
+                    password: hashedPassword
+                })
+                return user.save()
+                    .then(result => {
+                        console.log(result)
+                        return { ...result._doc, password: null }
+                    })
+                    .catch(err => console.log(err))
             })
             .catch(err => console.log(err))
     }
 }
+
