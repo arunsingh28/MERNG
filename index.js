774710855e9a9d91e610046dfddaa2c7432db0e7
Arun Singh <arunsingh2aug.as@gmail.com>
2021-05-29 18:16:23 +0530

    express-graphql

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 
-
 const port = process.env.PORT || 2
 
 app.listen(port,console.log(`server runing on port:${port}`))
