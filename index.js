b4abd2b098686c9ea62bba7180bd7bc7afd30e82
Arun Singh <arunsingh2aug.as@gmail.com>
2021-05-29 18:13:39 +0530

    setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+
+const port = process.env.PORT || 2
+
+app.listen(port,console.log(`server runing on port:${port}`))
